66f73ace7d569cdceefe298dbca1700536dfee0e
pathjo0826 <pathjo0826@skola.goteborg.se>
2022-03-26 17:23:30 +0100

    data access prod.class

--- src/se/yrgo/dataaccess/StudentDataAccessProdVersion.java
+++ src/se/yrgo/dataaccess/StudentDataAccessProdVersion.java
@@ -1,5 +1,45 @@
 package se.yrgo.dataaccess;
 
-public class StudentDataAccessProdVersion {
+import java.util.List;
 
+import javax.ejb.Stateless;
+import javax.enterprise.inject.Default;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import se.yrgo.domain.Student;
+
+@Stateless
+@Default
+public class StudentDataAccessProdVersion implements StudentDataAccess {
+	
+	@PersistenceContext
+	private EntityManager em;
+
+	@Override
+	public void insertStudent(Student newStudent) {
+		em.persist(newStudent);
+	}
+
+	@Override
+	public Student findStudentById(int id) {
+		TypedQuery<Student> tq1 = em.createQuery("select student from Student as student where student.id = :id", Student.class );
+		tq1.setParameter("id", id);
+		return tq1.getSingleResult();
+	}
+
+	@Override
+	public List<Student> findAllStudents() {
+		TypedQuery<Student> tq2 = em.createQuery("select student from Student as student", Student.class);
+		return tq2.getResultList();
+	}
+
+	@Override
+	public List<Student> findStudentByLastName(String lastName) {
+		TypedQuery<Student> tq3 = em.createQuery("select student from Student as student where student.lastName = :lastName", Student.class);
+		tq3.setParameter("lastName", lastName);
+		return tq3.getResultList();
+	}
+	
 }
